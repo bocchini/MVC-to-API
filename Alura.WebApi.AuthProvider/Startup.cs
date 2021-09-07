d9623a22808879b1405c6825631577e31a43f642
Américo L Bocchini <albocchini@gmail.com>
2021-09-07 18:15:41 -0300

    Separação do serviço API

--- Alura.WebApi.AuthProvider/Startup.cs
+++ Alura.WebApi.AuthProvider/Startup.cs
@@ -7,10 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Alura.WebApi.AuthProvider
 {
