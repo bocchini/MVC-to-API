7806c057a68e8a3b363560fcdb62bcdfad895782
Americo Bocchini <americo.bocchini@lambda3.com.br>
2021-09-08 17:32:40 -0300

    Alteração conexao ao bd de autenticação

--- Alura.WebApi.AuthProvider/LoginController.cs
+++ Alura.WebApi.AuthProvider/LoginController.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace Alura.ListaLeitura.Services
+namespace Alura.WebApi.AuthProvider
 {
     [ApiController]
     [Route("api/[controller]")]
